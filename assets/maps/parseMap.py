195025de0d5bfcf6948f6910043d60a5f92271a7
William Smith <liamcs98@gmail.com>
2017-01-12 14:09:15 -0500

    Fixing parsing so it actually works...hopefully

--- assets/maps/parseMap.py
+++ assets/maps/parseMap.py
@@ -8,16 +8,20 @@ def parseMapFile(mapName):
 	def actualParsingOfFile(filepath):
 		tempLineArray = []
 		with open(filepath, "r") as f: 
-			for line in f:
-				for x in line.split(","):
-					x.strip()
-					#I need to remove the " \n" but this no work at the moment
-					if x.find(" \\n", 0) > -1:
-						x = x.replace(" \\n", "")
+			mapCordArray.append([[blockType, int(xCord), int(yCord)] for blockType, xCord, yCord in [[element.strip() for element in line] for line in [line.split(',') for line in f]]])	
+			return mapCordArray
+			
+#This code didnt freaking work...........took me 2 hours to realise that >_<
+			# for line in f:
+			# 	for x in line.split(","):
+			# 		x.strip()
+			# 		#I need to remove the " \n" but this no work at the moment
+			# 		if x.find(" \\n", 0) > -1:
+			# 			x = x.replace(" \\n", "")
 
-					tempLineArray.append(x)
-				mapCordArray.append(tempLineArray)
-		return mapCordArray
+					#tempLineArray.append(x)
+				#mapCordArray.append(tempLineArray)
+			
 #All of this is to make sure that I'm giving the map name correctly
 #If not, it returns false, []
 	if os.path.exists(mapName):
@@ -32,8 +36,20 @@ def parseMapFile(mapName):
 		parseSucceed = True
 	else:
 		return parseSucceed, mapCordArray
-
 	return parseSucceed, mapCordArray
+
+def saveMapFile(mapName, mapCordArray):
+	print(mapCordArray)
+	#with open(mapName, "w") as f:
+	# for x in mapCordArray:
+	# 	tempArrayforLine =[]
+	# 	for i in x:
+	# 		for y in i:
+	# 			print(i)
+	# 			tempArrayforLine.append(i)
+			
+	# 		#f.write("%s,%i,%i \n" % (blockType, xCord, yCord))
+	# 	print(tempArrayforLine)
 
 if __name__ == '__main__':
 	# mapCordArray = []
@@ -49,4 +65,9 @@ if __name__ == '__main__':
 	# 			tempLineArray.append(x)
 	# 		mapCordArray.append(tempLineArray)
 
-	print(parseMapFile("map"))
+	temp = 0
+	temp2 = []
+
+	print(parseMapFile("map2.txt"))
+
+	# saveMapFile("map2.txt", temp2)
