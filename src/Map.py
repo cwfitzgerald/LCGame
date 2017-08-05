a07ee3665fc2257083804782e19b32ed4d558e91
liamcs98 <liamcs98@gmail.com>
2017-08-05 12:22:45 -0400

    cleanup

--- src/Map.py
+++ src/Map.py
@@ -58,19 +58,5 @@ level1.GenerateBasicFlatMap("a","g")
 print(level1.mapContents)
 level1.SaveMap()
 
-# if __name__ == '__main__':
-	
-# 	filename= input("Map name? (Please add file extension at the end =P)\n")
-# 	blockType = input("BlockName? (This will make the entire map this type)\n")
-# 	CurrentDirectory = os.path.dirname(os.path.realpath(__file__))
 
-# 	if os.path.exists(CurrentDirectory + "\%s" % (filename)):
-# 		if input("Are you sure? This will overwrite that file. [y,n]\n") == "y":
-# 			makeTheStuffInMapFile(filename, blockType)
-# 		else:
-# 			print("kk, I'll keep that alone.\n")
-# 			quit()
-# 	else:
-# 		print("No File found, I'll make you one though! =D\n")
-# 		makeTheStuffInMapFile(filename, blockType)
 
